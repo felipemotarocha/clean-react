a73dbbd20efc6ce62dbe967a1ae5f019c91fe32b
Felipe Rocha <felipemota.rocha@gmail.com>
2022-08-15 08:38:04 -0300

    test: ensure RemoteAuthentication throws UnexpectedError if HttpPostClient returns 400

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,6 +4,7 @@ import mockAuthentication from '@/domain/test/mock-authentication'
 import RemoteAuthentication from './remote-authentication'
 import InvalidCredentialsError from '@/domain/errors/invalid-credentials-error'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import UnexpectedError from '@/domain/errors/unexpected-error'
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -47,5 +48,15 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication())
 
     await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+  })
+
+  test('should throw UnexpectedError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+
+    httpPostClientSpy.response.statusCode = HttpStatusCode.badRequest
+
+    const promise = sut.auth(mockAuthentication())
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
