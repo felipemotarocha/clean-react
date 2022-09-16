5b073345f4f87df54b6f75f761ef3c3831b50003
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-15 22:04:52 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/data/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+import { HttpPostParams } from '@/data/protocols/http'
+
+export class AxiosHttpClient {
+  async post<B, R>(params: HttpPostParams<B>): Promise<R> {
+    return await axios.post(params.url)
+  }
+}
