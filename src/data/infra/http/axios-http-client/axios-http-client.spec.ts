9cc54ecf75238a73d27d2d542b4a7ec238f4399e
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-15 22:39:38 -0300

    test: ensure AxiosHttpClient post method returns HttpResponse

--- src/data/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/data/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,8 +5,13 @@ import { AxiosHttpClient } from './axios-http-client'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxiosPostResult = {
+  status: 201,
+  data: { param: 'any_value' },
+}
+mockedAxios.post.mockResolvedValue(mockedAxiosPostResult)
 
-const makeSut = (): AxiosHttpClient => {
+const makeSut = (): AxiosHttpClient<any, any> => {
   return new AxiosHttpClient()
 }
 
@@ -20,6 +25,24 @@ describe('AxiosHttpClient', () => {
     const body = { param: 'any_value' }
 
     sut.post({ url, body })
+
+    expect(postSpy).toHaveBeenCalledWith(url, body)
+  })
+
+  it('should return the correct status code and body', async () => {
+    const sut = makeSut()
+
+    const postSpy = jest.spyOn(mockedAxios, 'post')
+
+    const url = faker.internet.url()
+    const body = { param: 'any_value' }
+
+    const httpResponse = await sut.post({ url, body })
+
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosPostResult.status,
+      body: mockedAxiosPostResult.data,
+    })
 
     expect(postSpy).toHaveBeenCalledWith(url, body)
   })
