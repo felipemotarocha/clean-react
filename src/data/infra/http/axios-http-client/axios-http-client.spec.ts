4fd9b9587e1f888003ff77b08dbf31011d9fad03
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-15 22:32:29 -0300

    test: ensure AxiosHttpClient calls axios post method with correct body and url

--- src/data/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/data/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -11,15 +11,16 @@ const makeSut = (): AxiosHttpClient => {
 }
 
 describe('AxiosHttpClient', () => {
-  it('should call axios post method with correct url and post', () => {
+  it('should call axios post method with correct body and url', () => {
     const sut = makeSut()
 
     const postSpy = jest.spyOn(mockedAxios, 'post')
 
     const url = faker.internet.url()
+    const body = { param: 'any_value' }
 
-    sut.post({ url })
+    sut.post({ url, body })
 
-    expect(postSpy).toHaveBeenCalledWith(url)
+    expect(postSpy).toHaveBeenCalledWith(url, body)
   })
 })
