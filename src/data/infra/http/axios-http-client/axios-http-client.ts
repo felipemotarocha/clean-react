04f03187ef7cb7e28af2415747174a7c4b1c680b
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-15 22:32:05 -0300

    feat: ensure AxiosHttpClient calls axios post method with correct body and url

--- src/data/infra/http/axios-http-client/axios-http-client.ts
+++ src/data/infra/http/axios-http-client/axios-http-client.ts
@@ -4,6 +4,6 @@ import { HttpPostParams } from '@/data/protocols/http'
 
 export class AxiosHttpClient {
   async post<B, R>(params: HttpPostParams<B>): Promise<R> {
-    return await axios.post(params.url)
+    return await axios.post(params.url, params.body)
   }
 }
